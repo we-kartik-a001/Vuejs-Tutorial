f35942890193ae4a72eb58cb2648a55b3f6660f2
kartik aswal <kartik-a001@webreinvent.com>
2025-05-28 17:23:16 +0530

    ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import ComputedComponent from './components/ComputedComponent.vue';
+
+</script>
+
 <template>
   <div>
     <counter></counter>
     <template-syntax></template-syntax>
+    <ComputedComponent/>
   </div>
 </template>
 
