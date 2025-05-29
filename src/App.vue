3877daa94ced18f730950d961df7b013b528985f
kartik aswal <kartik-a001@webreinvent.com>
2025-05-29 10:28:20 +0530

    computed usage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
+import ClassBindings from './components/ClassBindings.vue';
 import ComputedComponent from './components/ComputedComponent.vue';
 
-</script>
+</script> -->
 
 <template>
   <div>
     <counter></counter>
     <template-syntax></template-syntax>
     <ComputedComponent/>
+    <ClassBindings class="foo bar"/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import App from './App.vue'
 import Counter from './components/Counter.vue';
 import TemplateSyntax from './components/TemplateSyntax.vue';
 import ComputedComponent from './components/ComputedComponent.vue';
+import ClassBindings from './components/ClassBindings.vue';
 
 const app = createApp(App);
 
 app.component('Counter', Counter);
 app.component('TemplateSyntax', TemplateSyntax);
 app.component('ComputedComponent', ComputedComponent);
+app.component('ClassBindings', ClassBindings);
 
 app.mount('#app'); 
 
