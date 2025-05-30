1dbc7147c10c3d5cd9fbf23434febc25f26bd65c
kartik aswal <kartik-a001@webreinvent.com>
2025-05-30 15:36:23 +0530

    lecture-6

---
+++ src/components/WatchComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>Ask yes no question ?</p>
+        <input type="text" v-model="question"/>
+        <p>{{ question }}</p>
+    </div>
+</template>
+<script>
+const question = ref(' ');
+</script>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Counter from './components/Counter.vue';
 import TemplateSyntax from './components/TemplateSyntax.vue';
+import WatchComponent from './components/WatchComponent.vue';
 
 const app = createApp(App);
 
 app.component('Counter', Counter);
 app.component('TemplateSyntax', TemplateSyntax);
+app.component('WatchComponent', WatchComponent);
 
 app.mount('#app'); 
 
