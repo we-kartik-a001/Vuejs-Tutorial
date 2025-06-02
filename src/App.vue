1d865e5bb3e5150a2e2cd6bb2305bb5b773094a1
kartik aswal <kartik-a001@webreinvent.com>
2025-06-02 15:10:04 +0530

    Vue lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
+<script setup>
+import { onBeforeUpdate, onUpdated, ref } from "vue";
+
+const showpractice = ref(true);
+
+const tagremover = () => {
+  if (showpractice.value == true) {
+    showpractice.value = false;
+  } else {
+    showpractice.value = true;
+  }
+};
+
+onBeforeUpdate(() => {
+  console.log(document.getElementById("practice"));
+  console.log("on before update");
+});
+
+onUpdated(() => {
+  console.log(document.getElementById("practice"));
+  console.log("on update");
+});
+
+</script>
+
 <template>
   <div>
     <counter></counter>
     <template-syntax></template-syntax>
-    <FormInput/>
-    <watch-component/>
-    <watch-effect/>
-    <Practice/>
-    <div style=" margin-bottom: 25rem;">
+    <FormInput />
+    <watch-component />
+    <watch-effect />
+    <Practice id="practice" v-if="showpractice" />
 
+    <div style="padding: 1rem 0">
+      <button @click="tagremover">Button</button>
     </div>
+    <div style="margin-bottom: 25rem"></div>
   </div>
 </template>
 
