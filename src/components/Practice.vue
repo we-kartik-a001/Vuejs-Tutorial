eee766ee0c4e814a3f0f96513c276e93b4653ae4
kartik aswal <kartik-a001@webreinvent.com>
2025-06-02 10:47:00 +0530

    Practice

--- src/components/Practice.vue
+++ src/components/Practice.vue
@@ -1,6 +1,10 @@
 <script setup>
 
 import {ref, watchEffect} from "vue";
+<<<<<<< feature/lecture-6-Form_Input_&_Watch_component
+=======
+
+>>>>>>> Practice
 const name = ref('John');
 
 watchEffect(() =>{
